a67d0876440e9629e406c6e10f5d08b2770684e9
kibougouz <kenta15901@yahoo.co.jp>
2020-03-04 16:28:42 +0900

    Add Test(テストに機能を追加)

---
+++ Assets/Test/TestScript/Canvas_ImageChange.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Canvas_ImageChange : MonoBehaviour
+{
+
+    Image my_Image;
+    //Set this in the Inspector
+    public Sprite[] my_Sprite;
+    public string keyname = "";
+    public int MaxResetCnt = 0;
+    private int Sprite_No;
+
+
+    void Start()
+    {
+        my_Image = GetComponent<Image>();
+        Sprite_No = 0;
+    }
+
+
+    void Update()
+    {
+        //文字列の名前のボタンが押されたらの処理
+        if (Input.GetButtonDown(""+keyname))
+        {
+            my_Image.sprite = my_Sprite[Sprite_No];
+            Sprite_No += 1;//ここで順番に表示させる為に＋している。
+            if (Sprite_No == MaxResetCnt) Sprite_No = 0;
+        }
+    }
+}
