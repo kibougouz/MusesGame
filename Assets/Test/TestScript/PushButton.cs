4c58be438c3180e5ef319fa45ba13bb988b015f5
Tousei-Umeno <60810234+Tousei-Umeno@users.noreply.github.com>
2020-03-10 14:38:45 +0900

    text

--- Assets/Test/TestScript/PushButton.cs
+++ Assets/Test/TestScript/PushButton.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PushButton : MonoBehaviour
 {
+    public Text text;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,7 @@ public class PushButton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //テキストを点滅させる処理
+        text.color = new Color(1, 1, 1, (Mathf.Sin(Time.time * 2) + 1f) * 0.5f);
     }
 }
