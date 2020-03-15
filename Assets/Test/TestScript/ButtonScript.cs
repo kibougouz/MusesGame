652d832fad126cb099a72dc426079cd2dde02be1
Tousei-Umeno <60810234+Tousei-Umeno@users.noreply.github.com>
2020-03-16 01:29:51 +0900

    tesuto

---
+++ Assets/Test/TestScript/ButtonScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public void Save()
+    {
+        Debug.Log("a");
+    }
+    public void Load()
+    {
+        Debug.Log("b");
+
+    }
+    public void Auto()
+    {
+        Debug.Log("c");
+
+    }
+    public void Log()
+    {
+        Debug.Log("d");
+
+    }
+    public void DigitalWatch()
+    {
+        Debug.Log("e");
+
+    }
+
+    //void Start()
+    //{
+
+    //}
+    //void Update()
+    //{
+
+    //}
+}
